2de8089ffe57997ba90c06610846de7825497859
iranjith <rgranjith89@gmail.com>
2023-10-26 06:45:31 +0530

    feat: responsive layout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { useState } from 'react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { useState } from "react";
+import { Button, ButtonGroup, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem pl="2" bg="orange.300" area={"nav"}>
+        Header
+      </GridItem>
+      <Show above="lg">
+        <GridItem pl="2" bg="pink.300" area={"aside"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
