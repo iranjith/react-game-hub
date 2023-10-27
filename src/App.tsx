b4058bc3f5431fd4a0f5a03427e756a680fd87a8
iranjith <rgranjith89@gmail.com>
2023-10-27 06:11:11 +0530

    feat: built color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,11 @@ function App() {
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem pl="2" bg="pink.300" area={"aside"}>
+        <GridItem pl="2" area={"aside"}>
           Aside
         </GridItem>
       </Show>
-      <GridItem pl="2" bg="green.300" area={"main"}>
+      <GridItem pl="2" area={"main"}>
         Main
       </GridItem>
     </Grid>
