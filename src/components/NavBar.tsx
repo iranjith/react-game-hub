e2ad03467ba49ed4bde8234ee8b4e28e7ff07756
iranjith <rgranjith89@gmail.com>
2023-12-08 06:24:34 +0530

    fix: search issues

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,16 @@ import React from "react";
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 
 
 const NavBar = () => {
   return (
     <HStack padding='1rem'>
-      <Image src={logo} boxSize="60px"></Image>
+      <Link to="/">
+        <Image src={logo} boxSize="60px" objectFit="cover"></Image>
+      </Link>
       <SearchInput></SearchInput>
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
