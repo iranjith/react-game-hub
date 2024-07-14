cda34d9f5d44e58db528e4e5fed7a7cc6dedbf49
Ranjith.Kumar <theranjith@gmail.com>
2024-07-14 06:24:03 +0530

    resolved import platform errors

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -11,32 +11,35 @@ import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon, Text } from "@chakra-ui/react";
 import { IconType } from "react-icons";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 
 interface Props {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
-
-    const iconMap : {[key:string]:IconType}={
-        // name: PlayStation
-        // slug: playstation
-        pc:FaWindows,
-        playstation: FaPlaystation,
-        xbox:FaXbox,
-        nintendo:SiNintendo,
-        mac: FaApple,
-        android: FaAndroid,
-        ios: MdPhoneIphone,
-        linux:FaLinux,
-        web:BsGlobe
-    }
+  const iconMap: { [key: string]: IconType } = {
+    // name: PlayStation
+    // slug: playstation
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    linux: FaLinux,
+    web: BsGlobe,
+  };
 
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon key={platform.id} as={iconMap[platform.slug]} color='gray.500'></Icon>
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color="gray.500"
+        ></Icon>
       ))}
     </HStack>
   );
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,19 +1,16 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "../hooks/usePlatforms";
-import { Platform } from "../entities/Platform";
 import usePlatform from "../hooks/usePlatform";
 import useGameQueryStore from "../store";
-
-
 
 //Notes: Platform selector component is in the GridItem component. Pass the platform selected input to the Game Grid component via App component.
 
 const PlatformSelector = () => {
   const { data, error } = usePlatforms();
-  const setSelectedPlatformId= useGameQueryStore(s=>s.setPlatformId)
-  const selectedPlatformId = useGameQueryStore(s=>s.gameQuery.platformId);
-  const selectedPlatform= usePlatform(selectedPlatformId);
+  const setSelectedPlatformId = useGameQueryStore((s) => s.setPlatformId);
+  const selectedPlatformId = useGameQueryStore((s) => s.gameQuery.platformId);
+  const selectedPlatform = usePlatform(selectedPlatformId);
 
   if (error) return null;
 
